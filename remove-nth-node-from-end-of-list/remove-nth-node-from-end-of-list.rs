7bba55cdc0f8b9c144a9e39a75264a5f7052ddea
Ziad Alzarka <ziadalzarka@gmail.com>
2024-03-28 15:06:20 +0200

    Rmove nth node from end of list

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.rs
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.rs
@@ -12,12 +12,14 @@ impl ListNode {
     }
 }
 
+// [1], 1
+// [1, 2, 3, 4, 5] 2
+
 pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
     let mut head = head;
 
     let mut fast_pointer = head.as_ref();
 
-    // Unwrap is safe because length of linked list is guaranteed to be > n
     for _ in 0..n {
         fast_pointer = fast_pointer.unwrap().next.as_ref();
     }
@@ -29,56 +31,43 @@ pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<Li
         fast_pointer = node.next.as_ref();
     }
 
+    let mut fast_pointer = head.as_mut();
     let mut slow_pointer = head.as_mut();
 
-    // Unwrap is safe because length of linked list is guaranteed to be > node_index
     for _ in 0..node_index - 1 {
         slow_pointer = slow_pointer.unwrap().next.as_mut();
+        fast_pointer = fast_pointer.unwrap().next.as_mut();
     }
 
-    // Node before removed node
-    let new_previous_pointer = slow_pointer;
+    // Target node
+    fast_pointer = fast_pointer.unwrap().next.as_mut();
 
-    dbg!(new_previous_pointer.as_ref());
+    // Next node
+    let next_node = fast_pointer.unwrap().next.take();
 
-    // Unwrap is safe because the node is guaranteed to exist
-    let removed_pointer = new_previous_pointer
-        .as_ref()
-        .unwrap()
-        .next
-        .as_ref();
-
-    dbg!(removed_pointer);
-
-    // // Must match because removed node might be the last node
-    // let new_next_pointer = match removed_pointer.next.as_ref() {
-    //     Some(next_pointer) => next_pointer.next.as_ref(),
-    //     None => None,
-    // };
-
-    // new_previous_pointer.unwrap().next = Some(new_next_pointer.unwrap().to_owned());
+    slow_pointer.unwrap().next = next_node;
 
     head
 }
 
 fn main() {
-    // let list = Some(Box::new(ListNode {
-    //     val: 1,
-    //     next: Some(Box::new(ListNode {
-    //         val: 2,
-    //         next: Some(Box::new(ListNode {
-    //             val: 3,
-    //             next: Some(Box::new(ListNode {
-    //                 val: 4,
-    //                 next: Some(Box::new(ListNode::new(5))),
-    //             })),
-    //         })),
-    //     })),
-    // }));
+    let list = Some(Box::new(ListNode {
+        val: 1,
+        next: Some(Box::new(ListNode {
+            val: 2,
+            next: Some(Box::new(ListNode {
+                val: 3,
+                next: Some(Box::new(ListNode {
+                    val: 4,
+                    next: Some(Box::new(ListNode::new(5))),
+                })),
+            })),
+        })),
+    }));
 
-    let list = Some(Box::new(ListNode { val: 1, next: None }));
+    // let list = Some(Box::new(ListNode { val: 1, next: None }));
 
-    let result = remove_nth_from_end(list, 1);
+    let result = remove_nth_from_end(list, 2);
 
     dbg!(result);
 }
